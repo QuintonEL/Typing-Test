286feb7b012166444abb39926927379b7b9f3e2c
QuintonEL <quintonlaborde@gmail.com>
2020-04-22 16:43:38 -0600

    added more texts

--- src/components/getText.js
+++ src/components/getText.js
@@ -10,7 +10,29 @@ export default () => {
     
     `Twelve more years passed. Each year the Bagginses had given very lively combined birthday-parties at Bag End; but now it was understood that something quite exceptional was being planned for that autumn. Bilbo was going to be eleventy-one, 111, a rather curious number, and a very respectable age for a hobbit (the Old Took himself had only reached 130); and Frodo was going to be thirty- three, 33, an important number: the date of his "coming of age".`,
     
-    "Tongues began to wag in Hobbiton and Bywater; and rumour of the coming event travelled all over the Shire. The history and character of Mr. Bilbo Baggins became once again the chief topic of conversation; and the older folk suddenly found their reminiscences in welcome demand."
+    "Tongues began to wag in Hobbiton and Bywater; and rumour of the coming event travelled all over the Shire. The history and character of Mr. Bilbo Baggins became once again the chief topic of conversation; and the older folk suddenly found their reminiscences in welcome demand.",
+
+    `"Elves and Dragons! I says to him. "Cabbages and potatoes are better for me and you. Don't go getting mixed up in the business of your betters, or you'll land in trouble too big for you," I says to him. And I might say it to others," he added with a look at the stranger and the miller.`,
+
+    `But the Gaffer did not convince his audience. The legend of Bilbo's wealth was now too firmly fixed in the minds of the younger generation of hobbits.`,
+
+    `"Ah, but he has likely enough been adding to what he brought at first," argued the miller, voicing common opinion. "He's often away from home. And look at the outlandish folk that visit him: dwarves coming at night, and that old wandering conjuror, Gandalf, and all. You can say what you like, Gaffer, but Bag End's a queer place, and its folk are queerer."`,
+
+    `"And you can say what you like, about what you know no more of than you do of boating, Mr. Sandyman," retorted the Gaffer, disliking the miller even more than usual. "If that's being queer, then we could do with a bit more queerness in these parts. There's some not far away that wouldn't offer a pint of beer to a friend, if they lived in a hole with golden walls. But they do things proper at Bag End. Our Sam says that everyone's going to be invited to the party, and there's going to be presents, mark you, presents for all - this very month as is."`,
+
+    "That very month was September, and as fine as you could ask. A day or two later a rumour (probably started by the knowledgeable Sam) was spread about that there were going to be fireworks - fireworks, what is more, such as had not been seen in the Shire for nigh on a century, not indeed since the Old Took died.",
+
+    "Days passed and The Day drew nearer. An odd-looking waggon laden with odd-looking packages rolled into Hobbiton one evening and toiled up the Hill to Bag End. The startled hobbits peered out of lamplit doors to gape at it. It was driven by outlandish folk, singing strange songs: dwarves with long beards and deep hoods. A few of them remained at Bag End. At the end of the second week in September a cart came in through Bywater from the direction of the Brandywine Bridge in broad daylight. An old man was driving it all alone. He wore a tall pointed blue hat, a long grey cloak, and a silver scarf. He had a long white beard and bushy eyebrows that stuck out beyond the brim of his hat. Small hobbit-children ran after the cart all through Hobbiton and right up the hill. It had a cargo of fireworks, as they rightly guessed. At Bilbo's front door the old man began to unload: there were great bundles of fireworks of all sorts and shapes, each labelled with a large red G and the elf-rune, .",
+
+    `That was Gandalf's mark, of course, and the old man was Gandalf the Wizard, whose fame in the Shire was due mainly to his skill with fires, smokes, and lights. His real business was far more difficult and dangerous, but the Shire-folk knew nothing about it. To them he was just one of the "attractions" at the Party. Hence the excitement of the hobbit-children. "G for Grand!" they shouted, and the old man smiled. They knew him by sight, though he only appeared in Hobbiton occasionally and never stopped long; but neither they nor any but the oldest of their elders had seen one of his firework displays - they now belonged to the legendary past.`,
+
+    "When the old man, helped by Bilbo and some dwarves, had finished unloading. Bilbo gave a few pennies away; but not a single squib or cracker was forthcoming, to the disappointment of the onlookers.",
+
+
+
+    `"Run away now!" said Gandalf. "You will get plenty when the time comes." Then he disappeared inside with Bilbo, and the door was shut. The young hobbits stared at the door in vain for a while, and then made off, feeling that the day of the party would never come.`,
+
+    "Inside Bag End, Bilbo and Gandalf were sitting at the open window of a small room looking out west on to the garden. The late afternoon was bright and peaceful. The flowers glowed red and golden: snap-dragons and sun-flowers, and nasturtiums trailing all over the turf walls and peeping in at the round windows.",
   ];
   return texts[Math.floor(Math.random()*texts.length)];
 }
