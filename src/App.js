e3654662bc08a190d5a366c35ae3ec7790317995
QuintonEL <quintonlaborde@gmail.com>
2020-04-22 12:35:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='container mt-5 mb-5'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3'>
+            <textarea
+              className='form-control mb-3'
+              placeholder='Start typing...'
+            >
+
+            </textarea>
+
+          </div>
+
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
