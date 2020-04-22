a8415687a93a857867e893dd3be77f1bcc94c97e
QuintonEL <quintonlaborde@gmail.com>
2020-04-22 12:51:49 -0600

    added functionaity to restart button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component }from 'react';
-
 import Preview from './components/Preview'
 import Speed from './components/Speed';
 
+
+const initialState = {
+  text: 'test',
+  userInput: ''
+}
+
 class App extends Component {
+
+  state = initialState;
+
+  onRestart = () => {
+    this.setState(initialState);
+  }
+
+  onUserInputChange = (e) => {
+    const value = e.target.value;
+    this.setState({
+      userInput: value
+    });
+  }
+
   render() {
     return (
       <div className='container mt-5 mb-5'>
@@ -13,11 +32,13 @@ class App extends Component {
             <textarea
               className='form-control mb-3'
               placeholder='Start typing...'
+              value={this.state.userInput}
+              onChange={this.onUserInputChange}
             >
             </textarea>
             <Speed />
             <div className='text-right'>
-              <button className='btn btn-light'>Restart</button>
+              <button className='btn btn-light' onClick={this.onRestart}>Restart</button>
             </div>
           </div>
 
